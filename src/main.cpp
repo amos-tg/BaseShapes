bf45984e0487c127775258756d47ace7becd5db0
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2026-04-11 23:03:11 -0400

    construction tests
    tests constructors of Rectangle, Circle, and Square in accordance with the
    test plan checklist.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,54 @@
+#include "shapes.h"
 #include <iostream>
 
-int main(void) {
-  std::cout << "Hello, Amos!" << std::endl;
+using namespace std;
+
+const char 
+  *TEST_PASS { "PASSED" },
+  *CONSTRUCTION_TESTS { "Test (shapes.h objects constructors): " },
+  *POLYMORPHISM_TESTS { "Test (shapes.h objects polymorphism): " },
+  *SETTER_TESTS { "Test (shapes.h setters): " };
+
+// for all shapes.h object derivations of BasicShape 
+void construction_tests(void);
+void polymorphism_tests(void);
+void setter_tests(void);
+
+int main(void) 
+{
+  construction_tests();
+  polymorphism_tests();
+  setter_tests();     
 
   return 0;
+}
+
+void construction_tests(void)
+{
+  cout << CONSTRUCTION_TESTS;
+
+  // create two rectangle objects with diff args
+  Rectangle 
+    rect1 { 5, 10, "box1" },
+    rect2 { 11, 12, "box2" };
+  
+  // create two circle objects with diff args
+  Circle
+    circ1 { 1, 2, 15, "Circ1" },
+    circ2 { 2, 4, 22, "Circ2" };
+
+  // create one square object
+  Square square { 2, "Square" };
+
+  cout << TEST_PASS << endl;
+}
+
+void polymorphism_tests(void)
+{
+  
+}
+
+void setter_tests(void)
+{
+
 }
